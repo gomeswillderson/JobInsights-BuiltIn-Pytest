17b36999a601bee7a8d8afe80c9c92b1b0845c84
Will <gomeswillderson@gmail.com>
2023-02-28 10:02:42 -0300

    doc: PEP 257

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -5,6 +5,14 @@ import csv
 
 @lru_cache
 def read(path: str) -> List[Dict]:
+    """Reads a file from a given path and returns its contents.
+
+    :args:
+        path (str): Full path to file.
+
+    :returns:
+        list: List of rows as dicts.
+    """
     with open(path, mode="r") as file:
         csv_file = csv.DictReader(file)
         job_list = []
@@ -13,22 +21,19 @@ def read(path: str) -> List[Dict]:
 
     return job_list
 
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ---------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    raise NotImplementedError
-
 
 def get_unique_job_types(path: str) -> List[str]:
+    """Checks all different job types and returns a list of them.
+
+    :note:
+        Must call `read`.
+
+    :args:
+        path (str): Must be passed to `read`.
+
+    :returns:
+        list: List of unique job types.
+    """
     jobs = read(path)
     unique_jobs = []
 
@@ -38,24 +43,17 @@ def get_unique_job_types(path: str) -> List[str]:
 
     return unique_jobs
 
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
-
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
+    """Filters a list of jobs by job_type.
+
+    :args:
+        jobs (list): List of jobs to be filtered.
+        job_type (str): Job type for the list filter.
+
+    :returns:
+        list: List of jobs with provided job_type.
+    """
     filtered_jobs = []
 
     for job in jobs:
@@ -63,18 +61,3 @@ def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
             filtered_jobs.append(job)
 
     return filtered_jobs
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
