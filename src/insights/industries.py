08fbf2b861fa044a038add7c003a759a0356de6c
Will <gomeswillderson@gmail.com>
2023-03-01 18:20:09 -0300

    doc: industries.py PEP 257

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -3,33 +3,44 @@ from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
+    """Return a list of unique industries from the provided jobs data file.
+
+    Note:
+        Must call `read`.
+
+    Args:
+        path (str): The file path of the jobs data file to read.
+
+    Returns:
+        list of str: A list of unique industries present in the jobs data file.
+
+    Raises:
+        FileNotFoundError: If the specified file path does not exist.
+        ValueError: If the specified file does not have a valid format.
+    """
     jobs = read(path)
-    unique_industrie = []
+    unique_industries = []
 
     for job in jobs:
         industry = job['industry']
-        if industry and industry not in unique_industrie:
-            unique_industrie.append(industry)
+        if industry and industry not in unique_industries:
+            unique_industries.append(industry)
 
-    return unique_industrie
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    return unique_industries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
+    """Filter a list of job dictionaries by industry.
+
+    Args:
+        jobs (List[Dict]): A list of job dictionaries.
+        industry (str): The industry to filter by.
+
+    Returns:
+        List[Dict]: A filtered list of job dictionaries
+        with the specified industry.
+
+    """
     filtered_industries = []
 
     for job in jobs:
@@ -37,18 +48,3 @@ def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
             filtered_industries.append(job)
 
     return filtered_industries
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
