27ed9c13de68d1f31c37af0ee5809ae8633e3568
Will <gomeswillderson@gmail.com>
2023-03-01 20:43:02 -0300

    test: brazilian jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,8 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    path = "tests/mocks/brazilians_jobs.csv"
+    result = read_brazilian_file(path)
+    assert {"title": "Maquinista",
+            "salary": "2000", "type": "trainee"} in result
