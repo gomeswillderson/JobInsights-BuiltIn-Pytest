152a40113e06763802cf5d765c118582eaa0b59b
Will <gomeswillderson@gmail.com>
2023-03-02 13:42:06 -0300

    test: commenting and extending tests

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -3,6 +3,18 @@ from src.pre_built.brazilian_jobs import read_brazilian_file
 
 def test_brazilian_jobs():
     path = "tests/mocks/brazilians_jobs.csv"
+    error = (
+        "read_brazilian_file() missing 1 required positional "
+        "argument: 'path'"
+    )
+
+    # Test calling function without argument
+    try:
+        read_brazilian_file()
+    except TypeError as e:
+        assert str(e) == error
+
+    # Test function with argument
     result = read_brazilian_file(path)
     assert {"title": "Maquinista",
             "salary": "2000", "type": "trainee"} in result
